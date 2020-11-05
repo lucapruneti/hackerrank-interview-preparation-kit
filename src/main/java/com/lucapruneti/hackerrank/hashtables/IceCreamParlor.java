7b2c4bc81c659c870d4aa948225c2fddfdc54ce8
Luca Pruneti <luca.pruneti@gmail.com>
2020-11-05 10:14:13 +0100

    Added IceCreamParlor challenge

--- src/main/java/com/lucapruneti/hackerrank/hashtables/IceCreamParlor.java
+++ src/main/java/com/lucapruneti/hackerrank/hashtables/IceCreamParlor.java
@@ -35,48 +35,25 @@ public class IceCreamParlor {
         }
     }
 
-    private static void addFlavorToMenu(int cost, int idx, Map<Integer, List<Integer>> menu) {
-        if (!menu.containsKey(cost)) {
-            menu.put(cost, new ArrayList<>());
-        }
-        menu.get(cost).add(idx);
-    }
-
     /**
      * @param cost,  an array of integers representing price for a flavor
      * @param money, an integer representing the amount of money they have to spend
      */
     static Tuple whatFlavors(int[] cost, int money) {
 
-        Map<Integer, List<Integer>> menu = new HashMap<>();
+        Map<Integer, Integer> menu = new HashMap<>();
 
         for (int i = 0; i < cost.length; i++) {
-            addFlavorToMenu(cost[i], i + 1, menu);
+            int tmpCost = cost[i];
+            int diff = money - tmpCost;
+
+            if (menu.containsKey(diff)) {
+                System.out.println(menu.get(diff) + " " + (i + 1));
+                return new Tuple(menu.get(diff), i + 1);
+            } else
+                menu.put(tmpCost, i + 1);
         }
 
-        int splitMoney = money / 2;
-
-        if (money % 2 == 0)
-            if (menu.containsKey(splitMoney))
-                if (menu.get(splitMoney).size() > 1) {
-                    List<Integer> idx = menu.get(splitMoney);
-                    return new Tuple(idx.get(0), idx.get(1));
-                }
-
-        int idx1 = -1;
-        int idx2 = -1;
-
-        for (int i = money % 2 == 0 ? splitMoney - 1 : splitMoney, j = splitMoney + 1; i < money && j < money; i--, j++) {
-            if (menu.containsKey(i) && menu.containsKey(j)) {
-                //found solution
-                idx1 = menu.get(i).get(0);
-                idx2 = menu.get(j).get(0);
-                break;
-            }
-        }
-
-        System.out.println(Math.min(idx1, idx2) + " " + Math.max(idx1, idx2));
-
-        return new Tuple(Math.min(idx1, idx2), Math.max(idx1, idx2));
+        throw new UnsupportedOperationException("Not Found");
     }
 }
