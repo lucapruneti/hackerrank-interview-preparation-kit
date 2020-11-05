102f43bb603d0a000da663453dbe7d5a164ddff8
Luca Pruneti <luca.pruneti@gmail.com>
2020-11-05 16:15:32 +0100

    Added LeftRotation challenge

--- src/main/java/com/lucapruneti/hackerrank/arrays/LeftRotation.java
+++ src/main/java/com/lucapruneti/hackerrank/arrays/LeftRotation.java
@@ -2,19 +2,6 @@ package com.lucapruneti.hackerrank.arrays;
 
 public class LeftRotation {
 
-    static void swap(int idx1, int idx2, int[] a) {
-        int tmp1 = a[idx1];
-        int tmp2 = a[idx2];
-
-        a[idx2] = tmp1;
-        a[idx1] = tmp2;
-    }
-
-    static void rotate(int[] a) {
-        for (int i = 0; i < a.length - 1; i++)
-            swap(i, i + 1, a);
-    }
-
     /**
      * @param a, An array of integers
      * @param d, the number of rotations
@@ -22,17 +9,20 @@ public class LeftRotation {
      */
     static int[] rotLeft(int[] a, int d) {
 
-        for (int i = 0; i < d; i++)
-            rotate(a);
+        int[] b = new int[a.length];
+
+        for (int i = 0, j = d; i < a.length; i++, j++) {
+            b[i] = a[j % a.length];
+        }
 
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i : a) {
+        for (int i : b) {
             stringBuilder.append(i);
             stringBuilder.append(" ");
         }
 
         System.out.println(stringBuilder.toString().trim());
 
-        return a;
+        return b;
     }
 }
